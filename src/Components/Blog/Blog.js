7e3743562cede60777e43d546df3d218ee65476e
souravkap04 <skapri04@gmail.com>
2021-01-13 15:38:21 +0530

    get axios

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,13 +1,32 @@
 import React , {Component} from 'react';
 import Practice from '../Practice/Practice';
 import classes from './Blog.module.css';
-
+import axios from 'axios';
 
 class Blog extends Component{
+    state={
+        blogs:[],
+    }
+    componentDidMount(){
+       axios.get('https://jsonplaceholder.typicode.com/posts')
+         .then(response=>{
+             const blogs = response.data.slice(0,4);
+             const updatedBlog = blogs.map(blog=>{
+                return{
+                    ...blog,
+                }
+             })
+             this.setState({blogs:updatedBlog});
+             
+         })
+    }
     render(){
+        const blog = this.state.blogs.map(post=>{
+            return <Practice 
+                      title={post.title}/>
+        })
         return(<div className={classes.Blog}>
-            <Practice/>
-            <Practice/>
+            {blog}
         </div>)
     }
 }
--- src/Components/Practice/Practice.js
+++ src/Components/Practice/Practice.js
@@ -3,10 +3,11 @@ import classes from './Practice.module.css';
 
 
 class Practice extends Component{
+   
     render(){
         return(
-            <div className={classes.Blog} >
-                <h2>hello react</h2>
+            <div className={classes.Practice} >
+                <p>{this.props.title}</p>
             </div>
             
         )
